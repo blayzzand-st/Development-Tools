d2973a23b2344d1f8f2b643e2c70dcd5abac83c4
Blayzzand <112903020+blayzzand-st@users.noreply.github.com>
2022-12-05 02:37:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
             if command == "exit":
                 break
             elif command == "easter egg":
-                print("HHHHH")
+                print("HA")
             else:
                 a, op, b = command.split(' ')
                 print(calculator(a, op, b))
