c507877d97b1363ac0f8490f07b6f1987912ee4e
Blayzzand <112903020+blayzzand-st@users.noreply.github.com>
2022-12-05 11:45:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
             if command == "exit":
                 break
             elif command == "easter egg":
-                print("version 0.1")
+                print("version 0.2")
             else:
                 a, op, b = command.split(' ')
                 print(calculator(a, op, b))
